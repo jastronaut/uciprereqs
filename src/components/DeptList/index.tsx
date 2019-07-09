d240e78ba38290795483f81eac36e4d199ff050f
Jas Delgado <jas@jasdelgado.com>
2019-07-09 11:37:38 -0400

    Make things simpler by just using index file

--- src/components/DeptList/index.tsx
+++ src/components/DeptList/index.tsx
@@ -1,3 +1,33 @@
-import DeptList from './DeptList';
+
+import React from 'react';
+
+export const DEPTS = [
+    'CS',
+    'INF',
+];
+
+interface Props {
+    onSelect: (...args: any) => any;
+}
+
+const DeptList: React.FC<Props> = (props: Props) => {
+    const { onSelect } = props;
+    const renderDepts = (dept: string) => (
+        <option value={dept}>
+            {dept}
+        </option>
+    );
+    const depts = DEPTS.map((dept) => renderDepts(dept));
+
+    return (
+        <>
+        <label htmlFor="selectDept">Select Department</label><br />
+        <select onChange={(e) => onSelect(e)} >
+            <option value="" selected={true} disabled={true}>Departments</option>
+            { depts }
+        </select>
+        </>
+    );
+}
 
 export default DeptList;
