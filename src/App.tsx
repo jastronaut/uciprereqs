ec7e48f42fbab58ae0bf015782e9ee12fe13d340
Jas Delgado <jas@jasdelgado.com>
2019-07-09 12:04:37 -0400

    Correctly route with /dept/classnumber to access classinfo :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { withRouter, RouteComponentProps } from 'react-router';
+import { Route, Switch} from 'react-router-dom';
 import ClassList from './components/ClassList';
-import DeptList from './components/DeptList';
+import DeptList, { DEPTS } from './components/DeptList';
 import ClassInfo from './components/ClassInfo';
 import History from './components/History';
 import './App.css';
 
-interface Props {}
+interface Props extends RouteComponentProps {}
 interface State {
 	curDept: string;
 	curClass: string;
@@ -30,6 +31,7 @@ class App extends React.Component<Props, State> {
 	}
 
 	selectDept(dept: string) {
+		this.props.history.push(`/${dept}`);
 		this.setState({curDept: dept, curClass: ''});
 		this.getClassList(dept);
 	}
@@ -51,7 +53,7 @@ class App extends React.Component<Props, State> {
 	renderClassList() {
 		const classList = this.state.classList;
 		return (
-			classList.map((aClass) => (
+			classList.map((aClass: string) => (
 				<option value={aClass}>{aClass}</option>
 			))
 		);
@@ -69,7 +71,7 @@ class App extends React.Component<Props, State> {
 			history = (history.splice(0, indexCourse)).concat(history.splice(indexCourse + 1, history.length));
 			history.unshift(toAdd);
 		}
-		
+		this.props.history.push(`/${this.state.curDept}/${newClass}`);
 		this.setState({curClass: newClass, history: history});
 	}
 
@@ -82,6 +84,37 @@ class App extends React.Component<Props, State> {
 		this.selectClass(num);
 	}
 
+	displayClassList = () => {
+		if (this.props.history.location.pathname !== '/') {
+			const dept = this.props.history.location.pathname.split('/')[1];
+			if (DEPTS.includes(dept)) {
+				return <ClassList classes={this.state.classList} onSelect={this.onSelectClass} />
+			} else {
+				return <p className="has-text-danger">Please select a valid department.</p>
+			}
+		}
+		return null;
+	}
+
+	createRoutes = () => (
+		<Switch>
+			<Route path="/:dept/:num" render={({match}) => (
+				<ClassInfo
+					dept={match.params.dept}
+					num={match.params.num}
+					/>
+			)} />
+			<Route path="/" render={() => (
+				this.state.curClass !== '' ?
+				<ClassInfo
+					dept={this.state.curDept}
+					num={this.state.curClass}
+				/>
+				: null
+			)} />
+		</Switch>
+	)
+
 	render() {
 		return (
 				
@@ -89,30 +122,14 @@ class App extends React.Component<Props, State> {
 				<div className={`column is-one-quarter`}>
 					<DeptList onSelect={this.onSelectDept} />
 					<br />
-					{/* <Switch> */}
-						{/* <Route path="/:dept" render={({match}) => { 
-							// this.selectDept(match.params.dept);
-							return (
-							<ClassList classes={this.state.classList} onSelect={this.onSelectClass} />);
-						 }} />
-						<Route path="/:dept/:num" render={({match}) => {
-							// this.selectDept(match.params.dept);
-							return (
-							<ClassList classes={this.state.classList} onSelect={this.onSelectClass} />);
-						}} /> */}
-					{/* </Switch> */}
 					{
-						((this.state.curDept !== '') && <ClassList classes={this.state.classList} onSelect={this.onSelectClass} />)
+						this.displayClassList()
 					}
 					<History history={this.state.history} clickHistory={this.onClickHistory} />
 				</div>
 			<div className="column">
 				{
-					(this.state.curClass !== '') && (
-						<ClassInfo
-							dept={this.state.curDept}
-							num={this.state.curClass}
-						/>)
+					this.createRoutes()
 				}
 			</div>
 			</div>
@@ -121,4 +138,5 @@ class App extends React.Component<Props, State> {
 	}
 }
 
-export default App;
+const UCIPrereqs = withRouter(App);
+export default UCIPrereqs;
