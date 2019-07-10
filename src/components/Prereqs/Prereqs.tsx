0c691105cf81ee1167d354cbddb0673d66c4d6e7
Jas Delgado <jas@jasdelgado.com>
2019-07-10 18:29:38 -0400

    trying to render new prereqs format

--- src/components/Prereqs/Prereqs.tsx
+++ src/components/Prereqs/Prereqs.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
+import { CourseInfo } from '../../Interfaces';
 
 interface Props {
-    allPrereqs: Array<string>
+    prereqs: CourseInfo['prereqs'];
 }
 
+const renderPrereq = (prereq: string) => (
+    <tr key={prereq}><td>{prereq}</td></tr>
+);
+
+const renderPrereqGroup = (prereqGroup: { [title: string] : string } ) => {
+    const groupList: Array<JSX.Element> = [];
+    for (let [course, info] of Object.entries(prereqGroup)) {
+        groupList.push(renderPrereq(course + ' ' + info));
+    }
+    return groupList;
+}
+
+const renderPrereqs = (prereqs: CourseInfo['prereqs']) => (
+    prereqs.map((group) => (
+        typeof(group) === "string" ? (
+            renderPrereq(group)
+        ) : (
+            renderPrereqGroup(group) 
+        )
+    ))
+)
+
 const Prereqs: React.FC<Props> = (props: Props) => {
-    const { allPrereqs } = props;
-    const prereqsList = allPrereqs.map((p) => (<tr key={p}><td>{p}</td></tr>));
+    const { prereqs } = props;
     return (
         <div className={`box prereqs`}>
             <h3 className={`title is-3`}>Prerequisites</h3>
             <table className={`table is-hoverable`}>
                 <tbody>
-                    {prereqsList}
+                    {renderPrereqs(prereqs)}
                 </tbody>
             </table>
         </div>
