1d76653f9679f0fad48dd34bbac9127025bf3050
Jas Delgado <jas@jasdelgado.com>
2019-07-16 17:46:50 -0400

    add new header and line height for overflowing titles

--- src/styles/Type.tsx
+++ src/styles/Type.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { UCIGold } from './Colors';
 
 interface TypeProps {
     color?: string;
@@ -14,6 +15,7 @@ export const Heading = styled.h1<TypeProps>`
     color: ${props => props.color ? props.color : '#000'};
     font-size: 2rem;
     margin-bottom: 1.5rem;
+    line-height: 3rem;
 
     > * {
         display: inline;
@@ -29,6 +31,16 @@ export const Heading = styled.h1<TypeProps>`
 export const SectionHeader = styled.h2`
 `;
 
-export const Text = styled.p`
+interface TextProps {
+    indented?: boolean;
+}
+
+export const Text = styled.p<TextProps>`
     line-height: 1.5rem;
+    ${props => props.indented && "margin-left: 1.5em;"}
+`;
+
+export const SmallHeader = styled.h3`
+    font-weight: 700;
+    color: ${UCIGold};
 `;
