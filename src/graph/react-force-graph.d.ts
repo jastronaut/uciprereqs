e4e2e4f8bf9e7d0871675fa5d2172f2ba0fc1c12
Jas Delgado <jas@jasdelgado.com>
2019-07-24 00:59:09 -0400

    needed type specifications

---
+++ src/graph/react-force-graph.d.ts
@@ -0,0 +1,14 @@
+declare module 'react-force-graph' {
+    export const ForceGraph2D;
+    export interface CanvasGraphNode {
+        id: string;
+        x: number;
+        y: number;
+        group: number;
+        __indexColor: string;
+    };
+    export interface CanvasGraphLink {
+        source: string;
+        target: string;
+    };
+}
