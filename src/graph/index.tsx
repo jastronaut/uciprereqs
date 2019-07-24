c18ace494d9c9126f28f6441b9a7da07b0cddfed
Jas Delgado <jas@jasdelgado.com>
2019-07-24 00:57:56 -0400

    use d3 directed graph library for react :)

--- src/graph/index.tsx
+++ src/graph/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { fabric } from 'react-fabricjs';
-import { init_graph } from './graph';
+import { UCBlue, UCIGold } from '../styles/Colors';
+import { ForceGraph2D, CanvasGraphNode, CanvasGraphLink } from 'react-force-graph';
 // import { CourseInfo } from '../Interfaces';
 interface Props {}
 
@@ -14,15 +14,47 @@ interface Props {}
 
 const Graph: React.FC<Props> = (props: Props) => {
     const [ targetCourse, setTargetCourse ] = useState<string>('CS 134');
-    const [ prereqsList, setPrereqsList ] = useState<{ [title: string] : Array<string>}>({'CS 134': ['A', 'B', 'C']});
+    const [ graphData, setGraphData ] = useState<any>({"nodes": [], "links": []});
+    const [ hoverLink, setHoverLink ] = useState<any>(null);
+
     useEffect(() => {
        console.log('starting...');
-       const canvas = new fabric.Canvas('c');
-       init_graph(targetCourse, prereqsList, canvas);
+
+       fetch('test-d3-input.json')
+        .then(res => res.json())
+        .then(data => setGraphData(data));
     }, []);
 
+    const drawNode = (node: CanvasGraphNode, ctx: CanvasRenderingContext2D, globalScale: number) => {
+        const label = node.id;
+        const fontSize = 12/globalScale;
+        ctx.font = `${fontSize}px Sans-Serif`;
+        const textWidth = ctx.measureText(label).width;
+        const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize); // some padding
+        ctx.fillStyle = UCBlue;
+        ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, bckgDimensions[0], bckgDimensions[1]);
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillStyle='#fff';
+        ctx.fillText(label, node.x, node.y);
+    };
+
+    const handleLinkHover = (link: CanvasGraphLink) => {
+        setHoverLink(link);
+    };
+
     return (
-        <canvas id='c' />
+        <ForceGraph2D
+            graphData={graphData}
+            dagMode="td"
+            nodeAutoColorBy="group"
+            dagLevelDistance={15}
+            linkColor={() => UCIGold}
+            linkWidth={(link: CanvasGraphLink) => link === hoverLink ? 5 : 1}
+            linkDirectionalParticles={hoverLink ? 4 : 1}
+            nodeCanvasObject={drawNode}
+            onLinkHover={handleLinkHover}
+        />
     );
 };
 
