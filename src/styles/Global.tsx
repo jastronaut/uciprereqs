0e0dea396866b4dddfcd07e96c95533249a80b47
Jas Delgado <jas@jasdelgado.com>
2019-08-26 02:51:50 -0400

    because flex vertical centering wasnt working before

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -1,10 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    body {
+    html, body {
         font-family: Arial, Helvetica, sans-serif;
         margin: 0;
         padding: 0;
+        width: 100%;
+        min-height: 100%;
+        height: 100%;
+    }
+
+    #root {
+        min-height: 100%;
+        height: 100%;
     }
 `;
 
